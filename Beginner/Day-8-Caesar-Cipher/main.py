d249bf4efdf95aa40e5b37aef8a72088a665040c
Melyssa Ndukwu <melyssaramos01@gmail.com>
2025-09-30 13:18:09 -0300

    day 8 project

--- Beginner/Day-8-Caesar-Cipher/main.py
+++ Beginner/Day-8-Caesar-Cipher/main.py
@@ -1,11 +1,10 @@
+from logo import logo
 alphabet = [
     "a","b","c","d","e","f","g","h","i","j","k","l","m",
     "n","o","p","q","r","s","t","u","v","w","x","y","z"
 ]
 
-direction = input("Type 'encode to encrypt, type 'decode' to decrypt: ").lower()
-text = input("Type your message: ")
-shift = int(input("Type the shift number: "))
+print(logo)
 
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
@@ -13,9 +12,21 @@ def caesar(original_text, shift_amount, encode_or_decode):
         shift_amount *= -1
 
     for letter in original_text:
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
+        if letter not in alphabet:
+            output_text += letter
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
-caesar(text, shift, direction)
+while True:
+    direction = input("Type 'encode to encrypt, type 'decode' to decrypt: ").lower()
+    text = input("Type your message: ")
+    shift = int(input("Type the shift number: "))
+
+    caesar(text, shift, direction)
+
+    answer = input("Type 'yes' if you want to go again. Otherwise type 'no'.").lower()
+    if answer == "no":
+        break

7a86250ba268b5bafbfbe8f2c65cacefe7324556
Melyssa Ndukwu <melyssaramos01@gmail.com>
2025-09-30 12:53:41 -0300

    day 8 part 2 updated

--- Beginner/Day-8-Caesar-Cipher/main.py
+++ Beginner/Day-8-Caesar-Cipher/main.py
@@ -16,6 +16,6 @@ def caesar(original_text, shift_amount, encode_or_decode):
         shifted_position = alphabet.index(letter) + shift_amount
         shifted_position %= len(alphabet)
         output_text += alphabet[shifted_position]
-    print(f"Here is the result: {output_text}")
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
 
 caesar(text, shift, direction)

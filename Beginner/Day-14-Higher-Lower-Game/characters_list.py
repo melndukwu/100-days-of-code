32545b76df2bcae5e1422190d313b0b654fd5be1
Melyssa Ndukwu <melyssaramos01@gmail.com>
2025-10-12 12:43:24 -0300

    day 12 project

---
+++ Beginner/Day-14-Higher-Lower-Game/characters_list.py
@@ -0,0 +1,79 @@
+danmei_characters = [
+    {
+        "name": "Chu Wanning",
+        "height": 180,
+        "bio": "one of the most powerful cultivators and owner of three holy weapons."
+    },
+
+    {
+        "name": "Mo Ran",
+        "height": 189,
+        "bio": "a talented but rebellious cultivator from Sicheng Peak."
+    },
+
+    {
+        "name": "Shi Mei",
+        "height": 183,
+        "bio": "a kindle hearted and gentle disciple of Sicheng Peak."
+    },
+
+    {
+        "name": "Xue Meng",
+        "height": 178,
+        "bio": "the young master of Sicheng Peak."
+    },
+
+    {
+        "name": "Taxian Jun",
+        "height": 186,
+        "bio": "the feared future self of Mo Ran. Infamous for his cruelty and immense power."
+    },
+
+    {
+        "name": "Wei Wuxian",
+        "height": 185,
+        "bio": "the Yiling Patriarch and Lan Zhan's soulmate."
+    },
+
+    {
+        "name": "Jiang Cheng",
+        "height": 185,
+        "bio": "the temperamental sect leader of Yunmeng Jiang."
+    },
+
+    {
+        "name": "Xue Yang",
+        "height": 180,
+        "bio": "known for his twisted sense of fun and cruelty."
+    },
+
+    {
+        "name": "Nie Mingjue",
+        "height": 190,
+        "bio": "strong and imposing leader of Qinghe Nie."
+    },
+
+    {
+        "name": "Nie Huaisang",
+        "height": 178,
+        "bio": "sect leader of Qinghe Nie."
+    },
+
+    {
+        "name": "Wen Ning",
+        "height": 183,
+        "bio": "the gentle Ghost General. Kind hearted and loyal to Wei Wuxian."
+    },
+
+    {
+        "name": "He Yu",
+        "height": 189,
+        "bio": "the fourth case of a rare disease called psychological Ebola."
+    },
+
+    {
+        "name": "Xie Qingcheng",
+        "height": 180,
+        "bio": "a very independent and strong willed medical school professor."
+    }
+]
---
+++ Beginner/Day-14-Higher-Lower-Game/main.py
@@ -0,0 +1,38 @@
+import random
+import characters_list as c
+
+score = 0
+
+# picking two different characters
+a = random.choice(c.danmei_characters)
+b = random.choice(c.danmei_characters)
+while b == a or b["height"] == a["height"]:
+    b = random.choice(c.danmei_characters)
+
+should_continue = True
+print("Guess which Danmei character is taller!")
+while should_continue:
+
+    print(f"Compare A: {a['name']}, {a['bio']}")
+    print(f"Against B: {b['name']}, {b['bio']}")
+    answer = input("Who is taller? Type 'A' or 'B': ").lower()
+
+    def check_answer():
+        if a["height"] > b["height"]:
+            return answer == "a"
+        else:
+            return answer == "b"
+
+    if check_answer():
+        score += 1
+        print("\n" * 20)
+        print(f"You're right! Current score: {score}")
+
+        #next round
+        a = b
+        b = random.choice(c.danmei_characters)
+        while b == a or b["height"] == a["height"]:
+            b = random.choice(c.danmei_characters)
+    else:
+        print(f"Wrong! Final score: {score}")
+        should_continue = False
